793d7bc9277776b3366f81949e0d3f8e4a24fc89
Adrian <adferrer0611@gmail.com>
2024-01-30 13:02:56 -0400

    Comments added to the OpenApi Scheme

--- Program.cs
+++ Program.cs
@@ -20,30 +20,43 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//Adding Swagger Auth UI
 builder.Services.AddSwaggerGen(option =>
 {
+    //Specifying the version
     option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    //Adding the security definitions with the name of "Bearer"
     option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
+        //Will be ubicated in the Header of the HTTP Request
         In = ParameterLocation.Header,
+        //Description
         Description = "Please enter a valid token",
+        //nAME
         Name = "Authorization",
+        //Type of security Scheme
         Type = SecuritySchemeType.Http,
+        //Format of the bearer toekn
         BearerFormat = "JWT",
+        //Scheme
         Scheme = "Bearer"
     });
+    //
     option.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
         {
+            //New Security Scheme
             new OpenApiSecurityScheme
             {
                 Reference = new OpenApiReference
                 {
+                    //Reference set to a SecurityScheme reference type
                     Type=ReferenceType.SecurityScheme,
+                    //Reference set to "Bearer"
                     Id="Bearer"
                 }
             },
+            //This empty array indicates that the security scheme does not require any additional permission, else, we should specify them inside the array
             new string[]{}
         }
     });
